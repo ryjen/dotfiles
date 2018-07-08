8e1e5c36592d5dc30280a9358fb31b5ac70cd9a4
Ryan Jennings <ryan@coda.life>
2018-07-08 15:34:21 -0700

    Install oh-my-zsh by default if not already

--- zsh/.oh_my.zsh
+++ zsh/.oh_my.zsh
@@ -1,9 +1,17 @@
 # Path to your oh-my-zsh installation.
 export ZSH=$HOME/.oh-my-zsh
 
-source $ZSH/oh-my-zsh-theme.sh
+ZSH_INIT=$ZSH/oh-my-zsh.sh
 
-source $ZSH/oh-my-zsh-plugins.sh
+if ! [[ -f $ZSH_INIT ]]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    mv $HOME/.zshrc.pre-oh-my-zsh $HOME/.zshrc
+fi
 
-source $ZSH/oh-my-zsh.sh
+source $HOME/.oh_my_theme.zsh
+
+source $HOME/.oh_my_plugins.zsh
+
+source $ZSH_INIT
+
 
