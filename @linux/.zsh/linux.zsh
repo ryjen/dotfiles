436dfad1c9d200dd3f25847e12da75001522113e
Ryan Jennings <ryan@coda.life>
2018-08-13 19:38:18 -0700

    Updates to mutt and linux

--- @linux/.zsh/linux.zsh
+++ @linux/.zsh/linux.zsh
@@ -1,13 +1,15 @@
 # GNU core utilities
 eval `dircolors ~/.zsh/dircolors.256dark`      # colored ls
 
-if type xmodmap > /dev/null; then
-    xmodmap ~/.Xmodmap
+if [ -f ~/.Xmodmap ] && [ type xmodmap > /dev/null ]; then
+  xmodmap ~/.Xmodmap
 fi
 
 if type xdg-open > /dev/null; then
-    alias open='xdg-open'
+  alias open='xdg-open'
 fi 
 
-xinput set-prop "c0der78’s Mouse" 286 1 &> /dev/null
+if type xclip > /dev/null; then
+  alias pbcopy='xclip -selection clipboard'
+fi
 
