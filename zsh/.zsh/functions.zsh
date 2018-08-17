df8b63d0f56584710dc7bf526b1df953379d4c98
Ryan Jennings <rjennings@cvent.com>
2018-08-17 14:30:41 -0700

    More updates

--- zsh/.zsh/functions.zsh
+++ zsh/.zsh/functions.zsh
@@ -5,7 +5,6 @@ function TRAPALRM() {  # don't clear completion items on reset prompt
     fi
 }
 
-
 function dirwatch() {
     inotifywait -m $1 -e create -e move -e delete |
         while read thepath action file; do
@@ -14,12 +13,16 @@ function dirwatch() {
 }
 
 function where() {
-    find . -name \*$1\*
+    find . -name $@
 }
 
-#function ssh() {
-#	/usr/bin/ssh -t "$@" tmux new-session -A -s ryjen-session
-#}
+function test_network() {
+  nc -zw1 "google.com" 443 > /dev/null
+}
+
+function ssh() {
+	/usr/bin/ssh -t "$@" tmux new-session -A -s ryjen-session
+}
 
 function psql_history() {
     psql -U postgres -c "SELECT (pg_stat_file('base/'||oid ||'/PG_VERSION')).modification, datname FROM pg_database;"
