b7929766e3604d3d1230a696931f8a7087459429
Ryan Jennings <ryan@coda.life>
2018-09-12 00:15:16 -0700

    Updates for tmux/zsh

--- tmux/.zsh/tmux.zsh
+++ tmux/.zsh/tmux.zsh
@@ -1,5 +1,7 @@
+export DISABLE_AUTO_TITLE='true'
 
 
+## install plugin manager if needed
 if [ -d "$HOME/.tmux/plugins/tpm" ]; then
   return
 fi
@@ -8,11 +10,12 @@ if ! type git > /dev/null; then
   return
 fi
 
-if test_network; then
+test_network
+
+if [[ $? != 0 ]]; then
   return
 fi
 
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 
-export DISABLE_AUTO_TITLE='true'
 
