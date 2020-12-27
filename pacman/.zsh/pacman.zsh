71431e75d5fdf2c4a3746f62f785b7a1d90bae74
Ryan Jennings <ryan@micrantha.com>
2020-12-27 02:03:18 -0800

    update pacman script

--- pacman/.zsh/pacman.zsh
+++ pacman/.zsh/pacman.zsh
@@ -1,17 +1,41 @@
 
 packages=(
-  diff-so-fancy
+  bat
+  aerc
+  byobu
+  cowfortune
+  cowsay
+  docker
+  docker-compose
+  fzf
+  git
+  jq
+  jrnl
+  make
+  neovim
+  yarn
+  yay
+  pinentry
+  ranger
+  stow
+  tmux
+  xclip
+  zsh
+  yubico-pam
+  yubikey-manager
 )
 
 # check all installed
 # TODO: do this only once and unlink
-pacman -Qs ${packages[@]} >/dev/null
+pacman -Qs ${packages[@]} 2>&1 >/dev/null
 
 if [ $? -ne 0 ]; then
   echo "Installing system packages"
   sudo pacman -S ${packages[@]}
 
-  rm -f $HOME/.zsh/pacman.zsh
+  if [ $? -eq 0 ]; then
+    rm -f ${(%):-%N}
+  fi
 fi
 
 
