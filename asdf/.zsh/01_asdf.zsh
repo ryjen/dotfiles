06dd9345adf83bbd2ed99b78179ab610f1cae1e7
Ryan Jennings <ryan@micrantha.com>
2021-01-19 14:44:10 -0800

    add golang, micrantha configs
    update git config
    
    add stow aliases and asdf vars

--- asdf/.zsh/01_asdf.zsh
+++ asdf/.zsh/01_asdf.zsh
@@ -1,5 +1,14 @@
-if [ -d "${HOME}/.asdf" ]; then
-  source $HOME/.asdf/asdf.sh
-#  source $HOME/.asdf/completions/asdf.bash
+ASDF_DIR="/usr/local/src/vendor/asdf"
+ASDF_VERSION="v0.8.0"
+
+if [ ! -d $ASDF_DIR ]; then
+  git clone https://github.com/asdf-vm/asdf.git $ASDF_DIR --branch $ASDF_VERSION
 fi
+
+source $ASDF_DIR/asdf.sh
+
+fpath=(${ASDF_DIR}/completions $fpath)
+# initialise completions with ZSH's compinit
+autoload -Uz compinit
+compinit
 
