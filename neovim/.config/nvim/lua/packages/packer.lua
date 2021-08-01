532eb106ee5ae4111631dd52a8b4924cac3a8b05
Ryan Jennings <rybertjen@gmail.com>
2021-08-01 01:36:52 -0700

    update vim config for debug adapter protocol

--- neovim/.config/nvim/lua/packages/packer.lua
+++ neovim/.config/nvim/lua/packages/packer.lua
@@ -3,28 +3,31 @@ return require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  -- language server protocol
   use 'neovim/nvim-lspconfig'
   use 'kabouzeid/nvim-lspinstall' 
- 
+  use 'nvim-lua/completion-nvim'
+
+  -- default keyboard layout
   use 'jooize/vim-colemak'
 
+  -- debugger
   use 'mfussenegger/nvim-dap'
-  use "Pocco81/DAPInstall.nvim"
+  use 'Pocco81/DAPInstall.nvim'
+  use 'rcarriga/nvim-dap-ui'
+  use 'theHamsta/nvim-dap-virtual-text'
 
   --- themes
   use 'altercation/vim-colors-solarized'
 
   -- Load on a combination of conditions: specific filetypes or commands
   -- Also run code after load (see the 'config' key)
-  use {
-    'w0rp/ale',
-    ft = {'sh', 'zsh', 'bash', 'c', 'cpp', 'cmake', 'html', 'markdown', 'vim', 'go', 'js'},
-    cmd = 'ALEEnable',
-    config = 'vim.cmd[[ALEEnable]]'
-  }
-
-  use 'glepnir/lspsaga.nvim'
-  use { 'nvim-lua/completion-nvim' }
+  --use {
+  --  'w0rp/ale',
+  --  ft = {'sh', 'zsh', 'bash', 'c', 'cpp', 'cmake', 'html', 'markdown', 'vim', 'go', 'js'},
+  --  cmd = 'ALEEnable',
+  --  config = 'vim.cmd[[ALEEnable]]'
+  --}
 
   -- Fuzzy finder
   use {
