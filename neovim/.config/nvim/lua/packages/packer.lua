aa5d57abe95bf992afd36f3f171493e5f6630361
Ryan Jennings <rybertjen@gmail.com>
2021-07-31 22:41:44 -0700

    separate neovim and vim for 0.5.0
    updated config for nvim-0.5.0 is much better

---
+++ neovim/.config/nvim/lua/packages/packer.lua
@@ -0,0 +1,38 @@
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use 'neovim/nvim-lspconfig'
+  use 'kabouzeid/nvim-lspinstall' 
+ 
+  use 'jooize/vim-colemak'
+
+  --- themes
+  use 'altercation/vim-colors-solarized'
+
+  -- Load on a combination of conditions: specific filetypes or commands
+  -- Also run code after load (see the 'config' key)
+  use {
+    'w0rp/ale',
+    ft = {'sh', 'zsh', 'bash', 'c', 'cpp', 'cmake', 'html', 'markdown', 'vim', 'go', 'js'},
+    cmd = 'ALEEnable',
+    config = 'vim.cmd[[ALEEnable]]'
+  }
+
+  use 'glepnir/lspsaga.nvim'
+  use { 'nvim-lua/completion-nvim' }
+
+  -- Fuzzy finder
+  use {
+      'nvim-telescope/telescope.nvim',
+      requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+  }
+
+
+  -- Post-install/update hook with neovim command
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+end)
+
+
