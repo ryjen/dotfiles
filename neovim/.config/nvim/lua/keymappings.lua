814299ad4ca722360af852a865a34867db9b7d33
Ryan Jennings <rybertjen@gmail.com>
2021-09-17 11:29:23 -0700

    neovim: add lspking, update keymappings and dap
    tmux: trim plugins and config
    
    git: update modular user includes
    
    weechat: switch to libera from freenode
    
    add nicks to secure data
    
    neovim: expand fast navigation to other modes

--- neovim/.config/nvim/lua/keymappings.lua
+++ neovim/.config/nvim/lua/keymappings.lua
@@ -5,16 +5,31 @@ utils.map('v', '<Tab><Tab>', '<Esc>gV')
 utils.map('o', '<Tab><Tab>', '<Esc>')
 utils.map('n', '<Tab><Tab>', '<C-I>')
 
+--- faster navigation with CTRL
 utils.map('n', '<C-h>', '5h')
 utils.map('n', '<C-n>', '5j')
 utils.map('n', '<C-e>', '5k')
 utils.map('n', '<C-i>', '5l')
 
+utils.map('v', '<C-h>', '5h')
+utils.map('v', '<C-n>', '5j')
+utils.map('v', '<C-e>', '5k')
+utils.map('v', '<C-i>', '5l')
+
+--- faster navigation is normal navigation in insert mode
+utils.map('i', '<C-h>', '<Left>')
+utils.map('i', '<C-n>', '<Down>')
+utils.map('i', '<C-e>', '<Up>')
+utils.map('i', '<C-i>', '<Right>')
+
+utils.map('n', '<leader>t', ':NvimTreeToggle<CR>')
+
 --- start spell checker
 utils.map('n', '<C-P>', ':set spell spelllang=en_ca<CR>')
-utils.map('n', '<leader>P', ':set spell spelllang=en_ca<CR>')
-utils.map('n', '<leader>p', 'z=')
-utils.map('n', '<leader>g', 'zg')
+utils.map('n', '<leader>zP', ':set spell spelllang=en_ca<CR>')
+utils.map('n', '<leader>zp', 'z=')
+utils.map('n', '<leader>zg', 'zg')
+utils.map('n', '<leader>zw', 'zw')
 
 utils.map('n', '<leader>k', ':bp<CR>')
 utils.map('n', '<leader>m', ':bn<CR>')
