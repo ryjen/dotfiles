5f0f9b927e2097517059bc4755dac1f27b911e5f
Ryan Jennings <rybertjen@gmail.com>
2022-04-14 22:07:47 -0700

    update configuration for new installs

--- k8s/.zsh/k8s.zsh
+++ k8s/.zsh/k8s.zsh
@@ -1,4 +1,10 @@
-source <(kubectl completion zsh)
 
-alias k=kubectl
-compdef __start_kubectl k
+type kubectl >/dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+
+  source <(kubectl completion zsh)
+
+  alias k=kubectl
+  compdef __start_kubectl k
+fi
--- starship/.zsh/starship.zsh
+++ starship/.zsh/starship.zsh
@@ -1,1 +1,5 @@
-eval "$(starship init zsh)"
+type starship >/dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+  eval "$(starship init zsh)"
+fi
