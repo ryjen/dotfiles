d984ae3fef0b2e121f1f30484e9d3c07ff535227
Ryan Jennings <rybertjen@gmail.com>
2022-05-31 02:26:05 -0700

    Begining of ansible version

--- collections/ansible_collections/ryjen/dotfiles/roles/zsh/.zsh/00_functions.zsh
+++ collections/ansible_collections/ryjen/dotfiles/roles/zsh/.zsh/00_functions.zsh
@@ -32,3 +32,9 @@ function type_exists() {
   return $?
 }
 
+function checktor() {
+  curl --socks5 localhost:9050 --socks5-hostname localhost:9050 -s https://check.torproject.org/api/ip
+  if [ $? -ne 0 ]; then
+    echo "Not running"
+  fi
+}
