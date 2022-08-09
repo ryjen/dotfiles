997f1cd3b2125a31c1c85f3312d72f4c71fd4806
Ryan Jennings <rybertjen@gmail.com>
2022-08-09 01:48:14 -0700

    updated vim configuration
    added progress field to taskwarrior

--- neovim/.config/nvim/lua/keymappings.lua
+++ neovim/.config/nvim/lua/keymappings.lua
@@ -1,4 +1,4 @@
-local utils = require('utils')
+local utils = require('config.utils')
 
 utils.map('i', '<Tab><Tab>', '<Esc>')
 utils.map('v', '<Tab><Tab>', '<Esc>gV')
@@ -6,15 +6,11 @@ utils.map('o', '<Tab><Tab>', '<Esc>')
 utils.map('n', '<Tab><Tab>', '<C-I>')
 
 --- faster navigation with CTRL
-utils.map('n', '<C-h>', '5h')
-utils.map('n', '<C-n>', '5j')
-utils.map('n', '<C-e>', '5k')
-utils.map('n', '<C-i>', '5l')
-
-utils.map('v', '<C-h>', '5h')
-utils.map('v', '<C-n>', '5j')
-utils.map('v', '<C-e>', '5k')
-utils.map('v', '<C-i>', '5l')
+local modes = {'n', 'v'}
+utils.map(modes, '<C-h>', '5h')
+utils.map(modes, '<C-n>', '5j')
+utils.map(modes, '<C-e>', '5k')
+utils.map(modes, '<C-i>', '5l')
 
 --- faster navigation is normal navigation in insert mode
 utils.map('i', '<C-h>', '<Left>')
@@ -22,14 +18,9 @@ utils.map('i', '<C-n>', '<Down>')
 utils.map('i', '<C-e>', '<Up>')
 utils.map('i', '<C-i>', '<Right>')
 
-utils.map('n', '<leader>t', ':NvimTreeToggle<CR>')
-
 --- start spell checker
-utils.map('n', '<C-P>', ':set spell spelllang=en_ca<CR>')
-utils.map('n', '<leader>zP', ':set spell spelllang=en_ca<CR>')
-utils.map('n', '<leader>zp', 'z=')
-utils.map('n', '<leader>zg', 'zg')
-utils.map('n', '<leader>zw', 'zw')
+utils.map('n', '<F3>', ':set spell spelllang=en_ca<CR>')
+utils.map('i', '<F3>', '<C-O>:set spell spelllang=en_ca<CR>')
 
 utils.map('n', '<leader>k', ':bp<CR>')
 utils.map('n', '<leader>m', ':bn<CR>')
@@ -37,3 +28,5 @@ utils.map('n', '<leader>K', ':tabp<CR>')
 utils.map('n', '<leader>M', ':tabn<CR>')
 utils.map('n', '<leader>x', ':bd<CR>')
 utils.map('n', '<leader>X', ':tabclose<CR>')
+
+utils.map('n', '<leader><leader>', '<Cmd>Telescope frecency theme=get_dropdown<CR>')
