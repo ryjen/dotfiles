77271a8f22f8144c8a67b75bb955d2dd594ed884
Ryan Jennings <robert@ryanjennin.gs>
2022-09-17 01:52:41 -0700

    Fixup lsp cmp error

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -32,8 +32,8 @@ local function init()
 		requires = {
 			"L3MON4D3/LuaSnip",
 			{ "hrsh7th/cmp-buffer", after = "nvim-cmp" },
-			"hrsh7th/cmp-nvim-lsp",
-			--'hrsh7th/cmp-nvim-lsp-signature-help',
+			{ "hrsh7th/cmp-nvim-lsp", after = "nvim-cmp" },
+			{ "hrsh7th/cmp-nvim-lsp-signature-help", after = "nvim-cmp" },
 			{ "hrsh7th/cmp-path", after = "nvim-cmp" },
 			{ "hrsh7th/cmp-nvim-lua", after = "nvim-cmp" },
 			{ "saadparwaiz1/cmp_luasnip", after = "nvim-cmp" },
