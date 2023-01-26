3054c1c5e5a1f02144c16130797749c9867b59fe
Ryan Jennings <rybertjen@gmail.com>
2023-01-26 10:18:50 -0800

    updates to roles

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 case "$1" in
-  test)
-    shift
-    ansible-playbook -i inventory/test bootstrap.yml --ask-become-pass $@
-    ;;
-  install) 
-    ansible-playbook -i inventory bootstrap.yml --ask-become-pass $@
-    ;;
-  *)
-    echo "Syntax: $(basename $0) <command>"
-    echo ""
-    echo "  test    : test installation in vagrant machine"
-    echo "  install : install on current local machine"
-    echo ""
-    ;;
+test)
+	shift
+	ansible-playbook -i inventory/test bootstrap.yml --ask-become-pass "$@"
+	;;
+install)
+	ansible-playbook -i inventory bootstrap.yml --ask-become-pass "$@"
+	;;
+*)
+	echo "Syntax: $(basename "$0") <command>"
+	echo ""
+	echo "  test    : test installation in vagrant machine"
+	echo "  install : install on current local machine"
+	echo "  uninstall : uninstall on current local machine"
+	echo ""
+	;;
 esac
-
