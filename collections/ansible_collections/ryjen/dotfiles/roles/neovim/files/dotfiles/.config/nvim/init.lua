0247b7feb1e9d320e863a9f506bc58329759cf25
Ryan Jennings <rybertjen@gmail.com>
2023-03-29 22:06:27 -0700

    update nvim config

--- collections/ansible_collections/ryjen/dotfiles/roles/neovim/files/dotfiles/.config/nvim/init.lua
+++ collections/ansible_collections/ryjen/dotfiles/roles/neovim/files/dotfiles/.config/nvim/init.lua
@@ -1,26 +1,46 @@
+--require "impatient"
+local g = vim.g
+local cmd = vim.cmd
+local o, wo, bo = vim.o, vim.wo, vim.bo
+local utils = require 'config.utils'
+local opt = utils.opt
+local autocmd = utils.autocmd
+local map = utils.map
 local execute = vim.api.nvim_command
+
+-- Disable some built-in plugins we don't want
+local disabled_built_ins = {
+  'gzip',
+  'man',
+  'matchit',
+  'matchparen',
+  'shada_plugin',
+  'tarPlugin',
+  'tar',
+  'zipPlugin',
+  'zip',
+  'netrwPlugin',
+}
+
+for i = 1, 10 do
+  g['loaded_' .. disabled_built_ins[i]] = 1
+end
 
 -- Sensible defaults
 require('settings')
 
--- Auto install packer.nvim if not exists
-local install_path = vim.fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
-end
-vim.cmd [[packadd packer.nvim]]
+-- Commands
+cmd [[command! PackerInstall packadd packer.nvim | lua require('plugins').install()]]
+cmd [[command! PackerUpdate packadd packer.nvim | lua require('plugins').update()]]
+cmd [[command! PackerSync packadd packer.nvim | lua require('plugins').sync()]]
+cmd [[command! PackerClean packadd packer.nvim | lua require('plugins').clean()]]
+cmd [[command! PackerCompile packadd packer.nvim | lua require('plugins').compile()]]
+
 -- Auto compile when there are changes in plugins.lua
 vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' 
-
--- Install plugins
-require('packages')
 
 -- Key mappings
 require('keymappings')
 
--- Setup Lua language server using submodule
-require('lsp')
-
 -- Another option is to groups configuration in one folder
 require('config')
-
