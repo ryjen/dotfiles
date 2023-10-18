6939708db428ba8a2fbfa3eafab9673ce24b1b89
Ryan Jennings <rybertjen@gmail.com>
2023-10-18 00:34:55 -0700

    chore: update neovim config

--- collections/ansible_collections/ryjen/dotfiles/roles/neovim/files/dotfiles/.config/nvim/init.lua
+++ collections/ansible_collections/ryjen/dotfiles/roles/neovim/files/dotfiles/.config/nvim/init.lua
@@ -1,46 +1,40 @@
---require "impatient"
+require("impatient")
 local g = vim.g
 local cmd = vim.cmd
-local o, wo, bo = vim.o, vim.wo, vim.bo
-local utils = require 'config.utils'
-local opt = utils.opt
-local autocmd = utils.autocmd
-local map = utils.map
-local execute = vim.api.nvim_command
 
 -- Disable some built-in plugins we don't want
 local disabled_built_ins = {
-  'gzip',
-  'man',
-  'matchit',
-  'matchparen',
-  'shada_plugin',
-  'tarPlugin',
-  'tar',
-  'zipPlugin',
-  'zip',
-  'netrwPlugin',
+	"gzip",
+	"man",
+	"matchit",
+	"matchparen",
+	"shada_plugin",
+	"tarPlugin",
+	"tar",
+	"zipPlugin",
+	"zip",
+	"netrwPlugin",
 }
 
 for i = 1, 10 do
-  g['loaded_' .. disabled_built_ins[i]] = 1
+	g["loaded_" .. disabled_built_ins[i]] = 1
 end
 
 -- Sensible defaults
-require('settings')
+require("settings")
 
 -- Commands
-cmd [[command! PackerInstall packadd packer.nvim | lua require('plugins').install()]]
-cmd [[command! PackerUpdate packadd packer.nvim | lua require('plugins').update()]]
-cmd [[command! PackerSync packadd packer.nvim | lua require('plugins').sync()]]
-cmd [[command! PackerClean packadd packer.nvim | lua require('plugins').clean()]]
-cmd [[command! PackerCompile packadd packer.nvim | lua require('plugins').compile()]]
+cmd([[command! PackerInstall packadd packer.nvim | lua require('plugins').install()]])
+cmd([[command! PackerUpdate packadd packer.nvim | lua require('plugins').update()]])
+cmd([[command! PackerSync packadd packer.nvim | lua require('plugins').sync()]])
+cmd([[command! PackerClean packadd packer.nvim | lua require('plugins').clean()]])
+cmd([[command! PackerCompile packadd packer.nvim | lua require('plugins').compile()]])
 
 -- Auto compile when there are changes in plugins.lua
-vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' 
+cmd("autocmd BufWritePost plugins.lua PackerCompile")
 
 -- Key mappings
-require('keymappings')
+require("keymappings")
 
 -- Another option is to groups configuration in one folder
-require('config')
+require("config")
