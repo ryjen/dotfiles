b473b4015ba1f3bcc50e0d7129bbc4b0cf95de0d
Ryan Jennings <rybertjen@gmail.com>
2023-10-18 00:34:55 -0700

    chore: update neovim config

--- collections/ansible_collections/ryjen/dotfiles/roles/neovim/files/dotfiles/.config/nvim/lua/config/init.lua
+++ collections/ansible_collections/ryjen/dotfiles/roles/neovim/files/dotfiles/.config/nvim/lua/config/init.lua
@@ -1,3 +1,11 @@
+<<<<<<< Updated upstream
 require("config.colorscheme")
 require("config.completion")
 require("config.tree")
+||||||| Stash base
+=======
+require("config.colorscheme")
+require("config.completion")
+require("config.tabby")
+--require("config.tree")
+>>>>>>> Stashed changes
