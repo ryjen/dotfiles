d10d59a891574c9b44481459acecb44d524e3bf1
Ryan Jennings <rybertjen@gmail.com>
2026-05-06 23:31:10 -0700

    feat(home): add podman tooling

--- modules/home/common.nix
+++ modules/home/common.nix
@@ -1,12 +1,21 @@
 { pkgs, ... }:
+let
+  dockerCompat = pkgs.runCommandLocal "podman-docker-compat" { } ''
+    mkdir -p "$out/bin"
+    ln -s ${pkgs.podman}/bin/podman "$out/bin/docker"
+  '';
+in
 {
   home.packages = with pkgs; [
     bottom
+    dockerCompat
     gdu
     ripgrep
     fd
     lazygit
     lazydocker
+    podman
+    podman-compose
     tig
     sad
     jq
