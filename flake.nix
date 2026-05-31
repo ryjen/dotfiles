07e162493262415fbc3c4ee9b22efde447180164
Ryan Jennings <rybertjen@gmail.com>
2026-05-31 03:16:21 -0700

    fix: version mismatch  with home-manager

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "ryjen dotfiles with incremental NixOS and Home Manager migration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hermes-agent.url = "github:NousResearch/hermes-agent";
