931a47a0d2b8094b0d7fecb5beed6698c852252e
RJ <rybertjen@gmail.com>
2026-06-20 09:29:07 +0000

    Disable GRUB installation for verify host

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -13,6 +13,11 @@
   time.timeZone = "America/Vancouver";
   i18n.defaultLocale = "en_US.UTF-8";
 
+  # This host is evaluated by CI as a generic NixOS verification target.
+  # It is not an installer image and should not try to install GRUB to a
+  # real disk during evaluation.
+  boot.loader.grub.enable = false;
+
   users.users.${username} = {
     isNormalUser = true;
     description = username;
