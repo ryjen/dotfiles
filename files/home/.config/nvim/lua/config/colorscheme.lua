ce284dbd34f6c18967ef7921e02673f8a8e1a432
RJ <rybertjen@gmail.com>
2026-06-20 09:02:55 +0000

    Guard Neovim colorscheme loading

--- files/home/.config/nvim/lua/config/colorscheme.lua
+++ files/home/.config/nvim/lua/config/colorscheme.lua
@@ -1,4 +1,7 @@
-local utils = require('config.utils')
-utils.opt('termguicolors', true)
-vim.cmd 'colorscheme solarized'
+local utils = require("config.utils")
+utils.opt("termguicolors", true)
 
+local ok, err = pcall(vim.cmd.colorscheme, "solarized")
+if not ok then
+	vim.notify("solarized colorscheme unavailable: " .. err, vim.log.levels.WARN)
+end
