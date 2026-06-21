a75775986af28ac3b4e1eaf4a33955203a1925f4
RJ <rybertjen@gmail.com>
2026-06-21 04:58:43 +0000

    Use laptop Waybar config for nixos profile

--- home/ryjen/profiles/nixos.nix
+++ home/ryjen/profiles/nixos.nix
@@ -1,8 +1,10 @@
-{ ... }:
+{ lib, ... }:
 {
   dotfiles.agents.hermes.enable = true;
   dotfiles.profiles.workstation.enable = true;
   dotfiles.profiles.android.enable = true;
   dotfiles.profiles.micrantha.enable = true;
   dotfiles.hypr.adoptedProfile = "technetium";
+
+  xdg.configFile."waybar/config.jsonc".source = lib.mkForce ../../../files/home/.config/waybar/config-technetium.jsonc;
 }
