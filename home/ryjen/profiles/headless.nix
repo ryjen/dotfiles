5810df274ba516da96dc68416d69ac2e56ed60ca
RJ <rybertjen@gmail.com>
2026-06-21 06:46:37 +0000

    Add headless capability profile

---
+++ home/ryjen/profiles/headless.nix
@@ -0,0 +1,9 @@
+{ ... }:
+{
+  dotfiles.host.role = "headless";
+  dotfiles.host.graphical.enable = false;
+  dotfiles.host.laptop.enable = false;
+  dotfiles.profiles.workstation.enable = false;
+  dotfiles.profiles.android.enable = false;
+  dotfiles.profiles.micrantha.enable = false;
+}
