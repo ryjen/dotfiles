2fbe76f3d5e97c0ee990773eb5680b157b516bbb
RJ <rybertjen@gmail.com>
2026-06-21 06:14:55 +0000

    Guard user systemd units for WSL

--- modules/home/common.nix
+++ modules/home/common.nix
@@ -1,65 +1,76 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    bottom
-    gdu
-    ripgrep
-    fd
-    gnumake
-    lazygit
-    podman
-    podman-compose
-    tig
-    sad
-    jq
-    yq-go
-    htop
-    curl
-    wget
-    autojump
-    keychain
-    unzip
-    zip
-    tree
-    tree-sitter
-  ];
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+{
+  options.dotfiles.host.userSystemd.enable = lib.mkEnableOption "Home Manager user systemd units" // {
+    default = true;
+  };
 
-  xdg.configFile."containers/registries.conf".text = ''
-    unqualified-search-registries = ["docker.io"]
-  '';
-
-  xdg.configFile."containers/policy.json".text = builtins.toJSON {
-    default = [
-      {
-        type = "insecureAcceptAnything";
-      }
+  config = {
+    home.packages = with pkgs; [
+      bottom
+      gdu
+      ripgrep
+      fd
+      gnumake
+      lazygit
+      podman
+      podman-compose
+      tig
+      sad
+      jq
+      yq-go
+      htop
+      curl
+      wget
+      autojump
+      keychain
+      unzip
+      zip
+      tree
+      tree-sitter
     ];
-  };
 
-  systemd.user.sockets.podman = {
-    Unit = {
-      Description = "Podman API Socket";
-      Documentation = [ "man:podman-system-service(1)" ];
-    };
-    Socket.ListenStream = "%t/podman/podman.sock";
-    Install.WantedBy = [ "sockets.target" ];
-  };
+    xdg.configFile."containers/registries.conf".text = ''
+      unqualified-search-registries = ["docker.io"]
+    '';
 
-  systemd.user.services.podman = {
-    Unit = {
-      Description = "Podman API Service";
-      Requires = [ "podman.socket" ];
-      After = [ "podman.socket" ];
-      Documentation = [ "man:podman-system-service(1)" ];
+    xdg.configFile."containers/policy.json".text = builtins.toJSON {
+      default = [
+        {
+          type = "insecureAcceptAnything";
+        }
+      ];
     };
-    Service = {
-      Type = "exec";
-      ExecStart = "${pkgs.podman}/bin/podman system service";
-    };
-  };
 
-  programs.zoxide = {
-    enable = true;
-    enableZshIntegration = true;
+    systemd.user.sockets.podman = lib.mkIf config.dotfiles.host.userSystemd.enable {
+      Unit = {
+        Description = "Podman API Socket";
+        Documentation = [ "man:podman-system-service(1)" ];
+      };
+      Socket.ListenStream = "%t/podman/podman.sock";
+      Install.WantedBy = [ "sockets.target" ];
+    };
+
+    systemd.user.services.podman = lib.mkIf config.dotfiles.host.userSystemd.enable {
+      Unit = {
+        Description = "Podman API Service";
+        Requires = [ "podman.socket" ];
+        After = [ "podman.socket" ];
+        Documentation = [ "man:podman-system-service(1)" ];
+      };
+      Service = {
+        Type = "exec";
+        ExecStart = "${pkgs.podman}/bin/podman system service";
+      };
+    };
+
+    programs.zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+    };
   };
 }
