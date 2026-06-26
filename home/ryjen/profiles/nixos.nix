da4b7686b76a6bed8702a1e35b5c6ca6d0f93c85
RJ <rybertjen@gmail.com>
2026-06-25 22:23:21 -0700

    Enable Hermes for workstation profiles (#62)
    * Enable Hermes for workstation profiles
    
    * Keep workstation Hermes enablement minimal
    
    * Remove duplicate Hermes profile toggle

--- home/ryjen/profiles/nixos.nix
+++ home/ryjen/profiles/nixos.nix
@@ -5,7 +5,6 @@
   ];
 
   dotfiles.host.name = "nixos";
-  dotfiles.agents.hermes.enable = true;
   dotfiles.profiles.android.enable = true;
   dotfiles.profiles.micrantha.enable = true;
   dotfiles.hypr.adoptedProfile = "dubnium";
--- home/ryjen/profiles/workstation.nix
+++ home/ryjen/profiles/workstation.nix
@@ -6,4 +6,5 @@
 
   dotfiles.host.role = "workstation";
   dotfiles.profiles.workstation.enable = true;
+  dotfiles.agents.hermes.enable = true;
 }
