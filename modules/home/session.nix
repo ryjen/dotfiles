0afbea7322f9e411d3c86d0475ec55aa8fa9523c
RJ <rybertjen@gmail.com>
2026-06-26 00:57:04 -0700

    Normalize workstation cursor theme

--- modules/home/session.nix
+++ modules/home/session.nix
@@ -30,6 +30,20 @@
       wofi
     ];
 
+    home.pointerCursor = {
+      gtk.enable = true;
+      x11.enable = true;
+      package = pkgs.adwaita-icon-theme;
+      name = "Adwaita";
+      size = 24;
+    };
+
+    gtk.cursorTheme = {
+      package = pkgs.adwaita-icon-theme;
+      name = "Adwaita";
+      size = 24;
+    };
+
     home.sessionVariables = {
       GIT_EDITOR = "nvim";
       PAGER = "bat -p";
@@ -39,6 +53,8 @@
       MOZ_ENABLE_WAYLAND = "1";
       MOZ_DBUS_REMOTE = "1";
       GTK_CSD = "0";
+      XCURSOR_THEME = "Adwaita";
+      XCURSOR_SIZE = "24";
       QT_QPA_PLATFORM = "wayland";
       QT_QPA_PLATFORMTHEME = "qt5ct";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
