1429326e2d6e4c9dd6db337c5e6373e292c2bcb7
RJ <rybertjen@gmail.com>
2026-06-26 20:20:22 -0700

    Add C++ build tools to devtools (#68)

--- modules/home/devtools.nix
+++ modules/home/devtools.nix
@@ -1,10 +1,14 @@
 { pkgs, ... }:
 {
   home.packages = [
+    pkgs.cmake
     pkgs.ffmpeg
+    pkgs.gcc
     pkgs.gh
     pkgs.git
+    pkgs.gnumake
     pkgs.openssh
+    pkgs.pkg-config
     pkgs.python311
     pkgs.ripgrep
     pkgs.uv
