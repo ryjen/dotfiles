30e954be4eb0ec2ac7e27a0d78bd5155e99138d7
Ryan Jennings <rybertjen@gmail.com>
2026-06-26 17:31:05 -0700

    fix(configctl): pip globals contract

--- modules/home/configctl.nix
+++ modules/home/configctl.nix
@@ -2,7 +2,15 @@
   lib,
   ...
 }:
+let
+  contractsDir = ../../contracts/configctl/init;
+  contractFiles = lib.filterAttrs (
+    name: type: type == "regular" && lib.hasSuffix ".toml" name
+  ) (builtins.readDir contractsDir);
+in
 {
-  xdg.configFile."configctl/init.d/npm-globals.toml".source =
-    ../../contracts/configctl/init/npm-globals.toml;
+  xdg.configFile = lib.mapAttrs' (name: _type: {
+    name = "configctl/init.d/${name}";
+    value = { source = "${contractsDir}/${name}"; };
+  }) contractFiles;
 }
