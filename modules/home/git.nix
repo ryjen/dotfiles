0db0a36932031d682f467efb5faeba1dccd5c753
RJ <rybertjen@gmail.com>
2026-06-26 20:07:29 -0700

    Use renamed Git settings option

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -33,36 +33,36 @@ in
         path = "~/.config/git/conf.d/micrantha";
       }
     ];
-    aliases = {
-      ff = "flow feature";
-      wip = "!git add --all; git ci -m WIP";
-      co = "checkout";
-      blame = "blame -w -C -C -C";
-      wdiff = "diff --word-diff";
-      cob = "checkout -b";
-      last = "log -1 HEAD";
-      lbr = "for-each-ref --sort=-committerdate --format='%(refname:short)' refs/heads/ | head -n 1";
-      st = "status";
-      fpush = "push --force-with-lease";
-      stash = "stash --all";
-      save = "commit -a";
-      aliases = "config --get-regex alias";
-      loga = "log --pretty=format:'%C(yellow)%h %<(24)%C(red)%ad %<(18)%C(green)%an %C(reset)%s' --date=local --max-count=10";
-      lg = "log --graph --abbrev-commit --decorate --date=relative --all";
-      changelog = "log origin..HEAD --format='* %s%n%w(,4,4)%+b'";
-      glg = "log --oneline --decorate --all --graph";
-      undo = "reset HEAD~1 --mixed";
-      correct = "reset --hard";
-      uncommit = "reset --soft HEAD~";
-      done = "push origin HEAD";
-      unstage = "reset HEAD --";
-      del = "branch -D";
-      br = "branch --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) [%(authorname)]' --sort=-committerdate";
-      cleanup-merged = "!f(){ git fetch && git branch --merged | grep -v '* ' | xargs git branch --delete; };f";
-      amend = "commit --amend --reuse-message=HEAD";
-      fixup = "rebase -i HEAD~2";
-    };
-    extraConfig = {
+    settings = {
+      alias = {
+        ff = "flow feature";
+        wip = "!git add --all; git ci -m WIP";
+        co = "checkout";
+        blame = "blame -w -C -C -C";
+        wdiff = "diff --word-diff";
+        cob = "checkout -b";
+        last = "log -1 HEAD";
+        lbr = "for-each-ref --sort=-committerdate --format='%(refname:short)' refs/heads/ | head -n 1";
+        st = "status";
+        fpush = "push --force-with-lease";
+        stash = "stash --all";
+        save = "commit -a";
+        aliases = "config --get-regex alias";
+        loga = "log --pretty=format:'%C(yellow)%h %<(24)%C(red)%ad %<(18)%C(green)%an %C(reset)%s' --date=local --max-count=10";
+        lg = "log --graph --abbrev-commit --decorate --date=relative --all";
+        changelog = "log origin..HEAD --format='* %s%n%w(,4,4)%+b'";
+        glg = "log --oneline --decorate --all --graph";
+        undo = "reset HEAD~1 --mixed";
+        correct = "reset --hard";
+        uncommit = "reset --soft HEAD~";
+        done = "push origin HEAD";
+        unstage = "reset HEAD --";
+        del = "branch -D";
+        br = "branch --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) [%(authorname)]' --sort=-committerdate";
+        cleanup-merged = "!f(){ git fetch && git branch --merged | grep -v '* ' | xargs git branch --delete; };f";
+        amend = "commit --amend --reuse-message=HEAD";
+        fixup = "rebase -i HEAD~2";
+      };
       core.excludesfile = "~/.gitignore";
       difftool.prompt = false;
       gui.gcwarning = false;
