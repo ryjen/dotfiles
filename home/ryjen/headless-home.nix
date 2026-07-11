b0ff2712651b79853db17b90cad7a8edeb7694ee
RJ <rybertjen@gmail.com>
2026-07-10 19:48:24 -0700

    Use optional local user selector

--- home/ryjen/headless-home.nix
+++ home/ryjen/headless-home.nix
@@ -1,13 +1,14 @@
 {
   username,
+  lib,
   ...
 }:
 {
   imports = [
     ./layers/lightweight.nix
     ./profiles/headless.nix
-    ./user.nix
-  ];
+  ]
+  ++ lib.optional (builtins.pathExists ./user.local.nix) ./user.local.nix;
 
   home.username = username;
   home.homeDirectory = "/home/${username}";
