1f13701ec8fc340b2b44efdf2857717fe23c3403
RJ <rybertjen@gmail.com>
2026-07-10 19:35:18 -0700

    Import tracked user configuration

--- home/ryjen/wsl-home.nix
+++ home/ryjen/wsl-home.nix
@@ -1,14 +1,13 @@
 {
   username,
-  lib,
   ...
 }:
 {
   imports = [
     ./layers/lightweight.nix
     ./profiles/wsl.nix
-  ]
-  ++ lib.optional (builtins.pathExists ./user.nix) ./user.nix;
+    ./user.nix
+  ];
 
   home.username = username;
   home.homeDirectory = "/home/${username}";
