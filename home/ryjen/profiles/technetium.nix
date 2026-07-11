fc286429275a11c1a4e9c63fcc17d653fdfdcac5
RJ <rybertjen@gmail.com>
2026-07-11 01:48:16 -0700

    Keep host-specific Technetium selections authoritative

--- home/ryjen/profiles/technetium.nix
+++ home/ryjen/profiles/technetium.nix
@@ -8,7 +8,7 @@
   dotfiles.profiles.browser.enable = lib.mkDefault true;
   dotfiles.profiles.android.enable = lib.mkDefault false;
   dotfiles.profiles.micrantha.enable = lib.mkDefault false;
-  dotfiles.hypr.adoptedProfile = lib.mkDefault "technetium";
+  dotfiles.hypr.adoptedProfile = "technetium";
 
   xdg.configFile."waybar/config.jsonc".source =
     lib.mkForce ../../../files/home/.config/waybar/config-technetium.jsonc;
