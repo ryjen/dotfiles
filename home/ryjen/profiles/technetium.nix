f44bfbd308e11d4c0742e40e91ca423e741a8c45
RJ <rybertjen@gmail.com>
2026-07-11 01:41:12 -0700

    fix technetium local override defaults

--- home/ryjen/profiles/technetium.nix
+++ home/ryjen/profiles/technetium.nix
@@ -5,10 +5,10 @@
   ];
 
   dotfiles.host.name = "technetium";
-  dotfiles.profiles.browser.enable = true;
-  dotfiles.profiles.android.enable = false;
-  dotfiles.profiles.micrantha.enable = false;
-  dotfiles.hypr.adoptedProfile = "technetium";
+  dotfiles.profiles.browser.enable = lib.mkDefault true;
+  dotfiles.profiles.android.enable = lib.mkDefault false;
+  dotfiles.profiles.micrantha.enable = lib.mkDefault false;
+  dotfiles.hypr.adoptedProfile = lib.mkDefault "technetium";
 
   xdg.configFile."waybar/config.jsonc".source =
     lib.mkForce ../../../files/home/.config/waybar/config-technetium.jsonc;
