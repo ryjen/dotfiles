2fa0f391d80f2ed6eacd552ba6652a8b14bbae22
RJ <rybertjen@gmail.com>
2026-07-11 01:41:05 -0700

    fix profile defaults for local overrides

--- home/ryjen/profiles/dubnium.nix
+++ home/ryjen/profiles/dubnium.nix
@@ -1,19 +1,19 @@
-{ ... }:
+{ lib, ... }:
 {
   imports = [
     ./workstation.nix
   ];
 
   dotfiles.host.name = "dubnium";
-  dotfiles.profiles.browser.enable = true;
-  dotfiles.profiles.android.enable = false;
-  dotfiles.profiles.micrantha.enable = false;
-  dotfiles.profiles.office.enable = true;
-  dotfiles.opsCadence.enable = true;
-  dotfiles.hypr.adoptedProfile = "dubnium";
+  dotfiles.profiles.browser.enable = lib.mkDefault true;
+  dotfiles.profiles.android.enable = lib.mkDefault false;
+  dotfiles.profiles.micrantha.enable = lib.mkDefault false;
+  dotfiles.profiles.office.enable = lib.mkDefault true;
+  dotfiles.opsCadence.enable = lib.mkDefault true;
+  dotfiles.hypr.adoptedProfile = lib.mkDefault "dubnium";
 
   dotfiles.music = {
-    enable = true;
-    musicDirectory = "/mnt/isotope/Music";
+    enable = lib.mkDefault true;
+    musicDirectory = lib.mkDefault "/mnt/isotope/Music";
   };
 }
