12090c970d8e10c165d624f13030dea7bfc66d94
RJ <rybertjen@gmail.com>
2026-07-13 03:13:19 -0700

    feat(android): install Android agent CLI

--- modules/home/android.nix
+++ modules/home/android.nix
@@ -11,7 +11,28 @@
     home.packages = with pkgs; [
       android-studio
       android-tools
+      curl
     ];
+
+    home.activation.installAndroidAgentCli = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      target="$HOME/.local/bin/android"
+
+      if [ ! -x "$target" ]; then
+        mkdir -p "$(dirname "$target")"
+        tmp="$(${pkgs.coreutils}/bin/mktemp)"
+        trap '${pkgs.coreutils}/bin/rm -f "$tmp"' EXIT
+
+        ${pkgs.curl}/bin/curl \
+          --fail \
+          --silent \
+          --show-error \
+          --location \
+          https://dl.google.com/android/cli/latest/linux_x86_64/android \
+          --output "$tmp"
+
+        ${pkgs.coreutils}/bin/install -m 0755 "$tmp" "$target"
+      fi
+    '';
 
     xdg.configFile."zsh/config.d/android".source = ../../files/home/.config/zsh/config.d/android;
 
