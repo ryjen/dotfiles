20c3900d674ed23d94281db2108280398cc29798
RJ <rybertjen@gmail.com>
2026-07-20 23:10:48 -0700

    enable Bluetooth audio support

---
+++ modules/nixos/bluetooth.nix
@@ -0,0 +1,19 @@
+{
+  ...
+}:
+{
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
+  services.blueman.enable = true;
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+}
