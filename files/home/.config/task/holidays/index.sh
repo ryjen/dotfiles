1a11fc1ad3185c263230e5a96e800b9eec2b8f02
RJ <rybertjen@gmail.com>
2026-09-05 20:32:07 -0700

    style(shell): apply shfmt to maintained scripts

--- files/home/.config/task/holidays/index.sh
+++ files/home/.config/task/holidays/index.sh
@@ -3,7 +3,7 @@
 cd "$HOME/.config/task/holidays" || exit 1
 
 for file in holidays*.rc; do
-  locale=${file:9:5}
-  echo "$locale"
-  "$HOME/.task/bin/update-holidays.pl" --locale "$locale" --file "holidays.${locale}.rc"
+	locale=${file:9:5}
+	echo "$locale"
+	"$HOME/.task/bin/update-holidays.pl" --locale "$locale" --file "holidays.${locale}.rc"
 done
