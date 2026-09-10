90ea5092e3fcfed88822656f74b0f95b566d1da4
RJ <rybertjen@gmail.com>
2026-09-09 21:31:55 -0700

    feat(tidyfs): consume component-owned flake package

--- modules/home/tidyfs.nix
+++ modules/home/tidyfs.nix
@@ -2,17 +2,17 @@
   config,
   lib,
   pkgs,
+  tidyfs,
   ...
 }:
 let
   cfg = config.dotfiles.tidyfs;
+  tidyfsPackage = tidyfs.packages.${pkgs.stdenv.hostPlatform.system}.default;
 in
 {
   options.dotfiles.tidyfs.enable = lib.mkEnableOption "tidyfs filesystem cleanup CLI";
 
   config = lib.mkIf cfg.enable {
-    home.packages = [
-      (pkgs.callPackage ../../packages/tidyfs.nix { })
-    ];
+    home.packages = [ tidyfsPackage ];
   };
 }
