a6a8374b4e8572bcf1f1d9fa851b0cb948cd7a2b
RJ <rybertjen@gmail.com>
2026-09-23 12:53:53 -0700

    fix(meeting): retain Zoom Teams and phone camera on laptop profile

--- home/ryjen/profiles/laptop.nix
+++ home/ryjen/profiles/laptop.nix
@@ -10,6 +10,11 @@
       laptop.enable = true;
     };
     profiles.workstation.enable = true;
-    meeting.enable = lib.mkDefault true;
+    meeting = {
+      enable = lib.mkDefault true;
+      zoom.enable = lib.mkDefault true;
+      teams.enable = lib.mkDefault true;
+      phoneCamera.enable = lib.mkDefault true;
+    };
   };
 }
